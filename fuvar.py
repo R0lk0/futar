e258eb55c85baf6301d265bd36d08f5d704c7385
R0lk0 <116081448+R0lk0@users.noreply.github.com>
2024-04-26 11:35:52 +0200

    valtozasok

--- fuvar.py
+++ fuvar.py
@@ -8,9 +8,21 @@ class Fuvar:
         self.borravalo = borravalo 
         self.fizetesmod = fizetesmod
 
+def negyesfeladat():
+    fuvarokSzama = 0
+    bevetel = 0
+    for sor in f:
+        if Fuvar(sor[0]) == 6185:
+            fuvarokSzama += 1
+            bevetel += Fuvar(sor[5])
+
 fuvarok = []
 f = open('fuvar.csv', 'rt', encoding = 'utf-8')
 for sor in f:
-    fuvarok.append(sor)
+    f.readline()
+    sor = sor.strip()
+    sor = sor.split(';')
+    fuvarok.append(Fuvar(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5], sor[6]))
 
-print('3. feladat:', len(fuvarok) -1, 'fuvar')
+print('3. feladat:', , 'fuvar')
+print('4. feladat: ')
